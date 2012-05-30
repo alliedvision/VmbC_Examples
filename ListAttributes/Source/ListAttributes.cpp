a15fef2aa66c2f915e7a64356bcb5e1f351bfa60
Holger Eddelbüttel <Holger.Eddelbuettel@alliedvision.com>
2012-05-30 12:52:04 +0000

    Corrected some unit tests, removed a few typos

--- ListAttributes/Source/ListAttributes.cpp
+++ ListAttributes/Source/ListAttributes.cpp
@@ -238,7 +238,7 @@ void PrintFeatureImpact(VmbHandle_t aHandle,const VmbFeatureInfo_t* aInfo)
         VmbFeatureInfo_t lFeatures[256];
         VmbUint32_t      lFound;
 
-        printf("\tselectedFeatures\t= ");
+        printf("\taffectedFeatures\t= ");
 
         if(!VmbAffectedFeaturesList(aHandle,aInfo->name,lFeatures,256,&lFound,sizeof(VmbFeatureInfo_t)))
             for(VmbUint32_t i=0;i<lFound;i++)
