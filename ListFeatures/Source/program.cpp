7ca57f24cf82f7b22409979f89d9fa267e0edb0c
Marcus Pfeffer <Marcus.Pfeffer@alliedvision.com>
2013-01-09 10:35:08 +0000

    - VimbaC ListFeatures example made platform independent - VimbaCPP ListCameras example made platform independent - iPrivate member of class cHelper, cEventListener and cProducer of VimbaC initialized with NULL - iPrivate member of class cActiveScheduler of LLK initialized with NULL

--- ListFeatures/Source/program.cpp
+++ ListFeatures/Source/program.cpp
@@ -25,12 +25,11 @@
 
 =============================================================================*/
 
-#include <tchar.h>
 #include <iostream>
 
 #include <ListFeatures.h>
 
-int _tmain( int argc, _TCHAR* argv[] )
+int main( int argc, char* argv[] )
 {
     std::cout << std::endl;
     std::cout << "///////////////////////////////////////////" << std::endl;
