108691d4610ada79e1da8cfc201af6ea2afed9f2
Stephan von Weihe <Stefan.vonWeihe@alliedvision.com>
2014-02-13 14:28:13 +0000

    Discovering all GigE cameras is moved from VimbaC examples to the common folder

---
+++ Common/DiscoverGigECameras.c
@@ -0,0 +1,45 @@
+#include "DiscoverGigECameras.h"
+#include <stdio.h>
+#include <VimbaC/Include/VimbaC.h>
+
+#ifdef WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+/**Discover GigE cameras if GigE TL is present.
+*/
+
+VmbError_t DiscoverGigECameras()
+{
+    VmbError_t          err             = VmbErrorSuccess;
+    VmbBool_t           bIsGigE         = 0;
+
+    err = VmbFeatureBoolGet( gVimbaHandle, "GeVTLIsPresent", &bIsGigE );            // Is Vimba connected to a GigE transport layer?
+    if ( VmbErrorSuccess == err )
+    {
+        if( bIsGigE )
+        {
+            err = VmbFeatureCommandRun( gVimbaHandle, "GeVDiscoveryAllOnce");       // Send discovery packets to GigE cameras
+            if ( VmbErrorSuccess == err )
+            {
+#ifdef WIN32                                                                            // And wait for them to return
+                Sleep( 200 );
+#else
+                usleep( 200 * 1000 );
+#endif
+            }
+            else
+            {
+                printf( "Could not ping GigE cameras over the network. Reason: %d\n", err );
+            }
+        }
+    }
+    else
+    {
+        printf( "Could not query Vimba for the presence of a GigE transport layer. Reason: %d\n\n", err);
+    }
+
+    return err;
+}
---
+++ Common/DiscoverGigECameras.h
@@ -0,0 +1,8 @@
+#ifndef AVT_DISCOVER_GIGE_CAMERAS_H_
+#define AVT_DISCOVER_GIGE_CAMERAS_H_
+
+#include <VimbaC/Include/VimbaC.h>
+
+VmbError_t DiscoverGigECameras();
+
+#endif
