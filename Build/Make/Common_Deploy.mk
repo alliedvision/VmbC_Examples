4887a1477a66c6df8c169c43ea69c08d53d7883e
Tim Handschack <Tim.Handschack@alliedvision.com>
2014-05-08 08:40:25 +0000

    ADDED: Hard/soft-flat switches for deploy makefiles

--- Build/Make/Common_Deploy.mk
+++ Build/Make/Common_Deploy.mk
@@ -1,44 +1,62 @@
 UNAME		= $(shell uname -m)
 
 ifeq ($(UNAME),i386)
-ARCH			= x86
+ARCH		= x86
 AUTOWORDSIZE	= 32
+AUTOFLOATABI	= ignore
 endif
 ifeq ($(UNAME),i486)
-ARCH			= x86
+ARCH		= x86
 AUTOWORDSIZE	= 32
+AUTOFLOATABI	= ignore
 endif
 ifeq ($(UNAME),i586)
-ARCH			= x86
+ARCH		= x86
 AUTOWORDSIZE	= 32
+AUTOFLOATABI	= ignore
 endif
 ifeq ($(UNAME),i686)
-ARCH			= x86
+ARCH		= x86
 AUTOWORDSIZE	= 32
+AUTOFLOATABI	= ignore
 endif
 ifeq ($(UNAME),x86_64)
-ARCH			= x86
+ARCH		= x86
 AUTOWORDSIZE	= 64
+AUTOFLOATABI	= ignore
 endif
 ifeq ($(UNAME),amd64)
-ARCH			= x86
+ARCH		= x86
 AUTOWORDSIZE	= 64
+AUTOFLOATABI	= ignore
 endif
 ifeq ($(UNAME),armv6l)
-ARCH			= arm
+ARCH		= arm
 AUTOWORDSIZE	= 32
+AUTOFLOATABI	= soft
 endif
 ifeq ($(UNAME),armv7l)
-ARCH			= arm
+ARCH		= arm
 AUTOWORDSIZE	= 32
+AUTOFLOATABI	= soft
 endif
 
 #Possible word sizes: 32, 64
 WORDSIZE        = $(AUTOWORDSIZE)
+#Possible float abis: soft, hard
+FLOATABI	= $(AUTOFLOATABI)
 
 ifneq ($(WORDSIZE),32)
 ifneq ($(WORDSIZE),64)
 $(error Invalid word size set)
+endif
+endif
+
+ifneq ($(FLOATABI),soft)
+ifneq ($(FLOATABI),hard)
+ifneq ($(FLOATABI),ignore)
+$(error Invalid float abi set)
+endif
 endif
 endif
 
@@ -50,14 +68,24 @@ CXX             = g++
 MAKE            = make
 CP              = cp
 
+#Set word size on x86
 ifeq ($(ARCH),x86)
 ARCH_CFLAGS     = -m$(WORDSIZE)
 endif
 
-#Debug compile flags
-#CONFIG_CFLAGS  = -O0 -g
+#Configure compiler and linker for soft or hard-float build on ARM
+ifeq ($(ARCH),arm)
+ifeq ($(FLOATABI),soft)
+ARCH_CFLAGS	= -marm -mfloat-abi=soft -march=armv4t
+else ifeq ($(FLOATABI),hard)
+ARCH_CFLAGS	= -mthumb -mfloat-abi=hard -march=armv7
+endif
+endif
 
-#Release compile flags
-CONFIG_CFLAGS   = -O3
+ifeq ($(CONFIG),Debug)
+  CONFIG_CFLAGS         = -O0 -g
+else
+  CONFIG_CFLAGS         = -O3
+endif
 
 COMMON_CFLAGS   = $(CONFIG_CFLAGS) $(ARCH_CFLAGS) -fPIC
