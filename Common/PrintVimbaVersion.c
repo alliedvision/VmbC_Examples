5c95dcd4a1feb417762c9ae471c17955b91c1eea
Matthias Heidenreich <Matthias.Heidenreich@alliedvision.com>
2014-07-08 07:51:01 +0000

    change: uniform formating

--- Common/PrintVimbaVersion.c
+++ Common/PrintVimbaVersion.c
@@ -34,8 +34,8 @@
 */
 void PrintVimbaVersion()
 {
-    VmbVersionInfo_t version_info;
-    VmbError_t result = VmbVersionQuery( &version_info, sizeof( version_info ));
+    VmbVersionInfo_t    version_info;
+    VmbError_t          result = VmbVersionQuery( &version_info, sizeof( version_info ));
     if( VmbErrorSuccess == result)
     {
         printf("Vimba Version Major: %u Minor: %u Patch: %u\n", version_info.major, version_info.minor,version_info.patch);
