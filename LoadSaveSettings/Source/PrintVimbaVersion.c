a0ab2fb06ea3c2d4307afdffd2c559826a987d42
Sebastian Günther <Sebastian.Guenther@alliedvision.com>
2015-12-18 13:09:49 +0000

    -VimbaC load/save settings example added

---
+++ LoadSaveSettings/Source/PrintVimbaVersion.c
@@ -0,0 +1,47 @@
+/*=============================================================================
+  Copyright (C) 2014 Allied Vision Technologies.  All Rights Reserved.
+
+  Redistribution of this file, in original or modified form, without
+  prior written consent of Allied Vision Technologies is prohibited.
+
+-------------------------------------------------------------------------------
+
+  File:        PrintVimbaVersion.h
+
+  Description: Print Vimba version.
+
+-------------------------------------------------------------------------------
+
+  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF TITLE,
+  NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR  PURPOSE ARE
+  DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=============================================================================*/
+
+#include "PrintVimbaVersion.h"
+#include <stdio.h>
+#include <VimbaC/Include/VimbaC.h>
+
+// Purpose: Prints the Vimba version information.
+void PrintVimbaVersion()
+{
+    VmbVersionInfo_t    version_info;
+    VmbError_t          result = VmbVersionQuery( &version_info, sizeof( version_info ));
+    if( VmbErrorSuccess == result)
+    {
+        printf( "Vimba Version Major: %u Minor: %u Patch: %u\n", version_info.major, version_info.minor,version_info.patch );
+    }
+    else
+    {
+        printf( "VmbVersionQuery failed with Reason: %x", result );
+    }
+}
+
+
